0820d6eca29c0b326c194783971026a1a7a909f9
malheading <ss010510@gmail.com>
2021-04-18 13:35:16 +0900

    Part4 Finished

--- Messenger/Controllers/Login/LoginViewController.swift
+++ Messenger/Controllers/Login/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
@@ -132,6 +133,17 @@ class LoginViewController: UIViewController {
             return
         }
         //여기에 Firebase Log in을 적용할 예정//
+        //Firebase Log in을 했을 때
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: {authResult, error in
+            guard let result=authResult, error==nil else{
+                print("Failed to log in user with email:\(email)")
+                return
+            }
+            let user=result.user
+            print("Logged in with user: \(email)")
+            
+            
+        })
     }
     
     func alertUserLoginError(){
